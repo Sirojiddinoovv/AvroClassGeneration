dca06cf415f2ffbf8470bfacfa49aa7ed5ead351
Home-PC <davrbankit@gmail.com>
2025-05-25 15:49:50 +0500

    added avro dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
-
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1"
 }
 
 group = "uz.nodir"
@@ -34,11 +34,30 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.apache.avro:avro:1.12.0")
 }
 
 kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+
+avro {
+    setCreateSetters(true)
+    setFieldVisibility("private")
+    setOutputCharacterEncoding(Charsets.UTF_8)
+    setStringType("String")
+    setEnableDecimalLogicalType(true)
+
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("src/main/kotlin")
+        }
     }
 }
 
